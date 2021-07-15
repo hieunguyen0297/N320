0116e968b3fbec850d3c76209f74732a22db0304
Hieu Nguyen <73616304+hieunguyen0297@users.noreply.github.com>
2021-07-15 12:26:17 -0400

    css animation

---
+++ labs/lab4/app.js
@@ -0,0 +1,21 @@
+gsap.from(".container", { x:-50, opacity: 0, ease: "power2.out", delay: 1.5});
+
+let purpleBox = document.getElementsByClassName("purpleBox");
+
+for(let i = 0; i < purpleBox.length; i++){
+    gsap.from(purpleBox[i], {opacity: 0, delay: Math.random() + 1.7});
+
+    purpleBox[i].addEventListener("mouseover", (event) => {
+        event.target.innerHTML = i + 1;
+        gsap.to(purpleBox[i], {duration: 0.7, rotation: 4,scale: 1.1});
+        gsap.to(purpleBox[i], { backgroundColor: "#6732d1"});
+    })
+
+    purpleBox[i].addEventListener("click", (event) => {
+        event.target.innerHTML = "";
+        gsap.to(purpleBox[i], {duration: 0.7, rotation: 0, scale: 1});
+        gsap.to(purpleBox[i], {backgroundColor: "rgb(154, 104, 200)"});
+           
+    })
+}
+
